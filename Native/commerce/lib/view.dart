2e228270332af550ef1d2a81a49e77500172aad1
BeomChul Kim <2017253083@yonsei.ac.kr>
2021-11-29 11:13:22 +0900

    native

---
+++ Native/commerce/lib/view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'package:webview_flutter/webview_flutter.dart';
+
+class View extends StatefulWidget {
+  @override
+  _ViewState createState() => _ViewState();
+}
+
+class _ViewState extends State<View> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebView(
+          initialUrl: 'http://3.35.120.54:8080/',
+          javascriptMode: JavascriptMode.unrestricted,
+        ),
+      ),
+    );
+  }
+}
