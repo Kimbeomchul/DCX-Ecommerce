1b626cb33492a176dc48a366f71402b95ffb9d32
BeomChul Kim <2017253083@yonsei.ac.kr>
2021-12-02 14:41:08 +0900

    DTO

--- backend/src/main/java/com/lotte/dto/UserDTO.java
+++ backend/src/main/java/com/lotte/dto/UserDTO.java
@@ -7,14 +7,15 @@ import lombok.Setter;
 @Setter
 public class UserDTO {
     private String member_id;
-    private String member_pw;
+    private String member_social;
     private String member_name;
     private String member_phone;
     private String member_address;
     private String member_level;
     private String member_regdate;
     private int member_savemoney;
-
+    private String member_email;
+    private String member_image;
 }
 
 
