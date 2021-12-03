4d85c89c2b9c08ad70162e8eada62bcbf60e3cf6
BeomChul Kim <2017253083@yonsei.ac.kr>
2021-12-03 10:15:59 +0900

    Update PayController.java

--- backend/src/main/java/com/lotte/controller/PayController.java
+++ backend/src/main/java/com/lotte/controller/PayController.java
@@ -5,6 +5,7 @@ import com.lotte.dto.PayDTO;
 import com.lotte.dto.ZzimDTO;
 import com.lotte.service.ItemService;
 import com.lotte.service.PayService;
+import com.lotte.service.UserService;
 import com.lotte.service.ZzimService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -17,11 +18,12 @@ import java.util.List;
 public class PayController {
 
     private final PayService payService;
+    private final UserService userService;
 
     // 결제내역 저장
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @RequestMapping(value = "/paysave", method = RequestMethod.POST)
-    public void addPay(@RequestParam(value="datas") String datas, @RequestParam(value="member_id") String member_id , @RequestParam(value="pay_kakao") String pay_kakao) {
+    public void addPay(@RequestParam(value="datas") String datas, @RequestParam(value="savemoney") int savemoney, @RequestParam(value="member_id") String member_id , @RequestParam(value="pay_kakao") String pay_kakao) {
 
 
         try {
@@ -29,6 +31,7 @@ public class PayController {
             for (int i = 0; i < list1.length; i++) {
                 payService.addPay(member_id, Integer.parseInt(list1[i]), pay_kakao);
             }
+            userService.addsave(savemoney,member_id);
         }catch (Exception e){
             System.out.println("Error with payment : < addPay > "+ e );
         }
