e2661d96b9c173f4e5357e2b2d7a30f8512cd8ef
15Z95N-G\LDCC <joohwan.jeon@lotte.net>
2021-12-08 16:31:40 +0900

    빈 페이지 추가요

---
+++ frontend/src/components/empty.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="empty">
+    <v-icon size="400%">{{iconName}}</v-icon>
+    <div>{{message}}</div>
+  </div>
+</template>
+
+<style scoped>
+  .empty {
+    position: absolute;
+    top: 40%;
+    text-align: center;
+    width: 100%;
+    font-weight: bold;
+  }
+</style>
+
+<script>
+export default {
+    components: {
+    },
+    props: ['iconName', 'message'],
+    data: () => ({
+      // books: store.state.books
+    }),
+    methods: {
+    },
+    created() {
+      console.log(this.test);
+    }
+  }
+</script>
